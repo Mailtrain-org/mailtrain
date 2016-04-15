b444440d65d65139e23960f734b8f540efe1fc99
Andris Reinman <andris@kreata.ee>
2016-04-14 22:12:21 -0700

    Updated nitrous install script

--- nitrous-post-create.sh
+++ nitrous-post-create.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
 
 export DEBIAN_FRONTEND=noninteractive
+
+sudo apt-add-respository ppa:chris-lea/redis-server
+
 sudo apt-get update
-sudo -E apt-get -q -y install mysql-server
+sudo -E apt-get -q -y install mysql-server pwgen redis-server
 sudo apt-get clean
 
-mysql -u root -e "CREATE USER 'mailtrain'@'localhost' IDENTIFIED BY 'mailtrain';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON mailtrain.* TO 'mailtrain'@'%' WITH GRANT OPTION;"
-mysql -u mailtrain --password="mailtrain" -e "CREATE database mailtrain;"
-mysql -u mailtrain --password="mailtrain" -D mailtrain < setup/mailtrain.sql
+MYSQL_PASSWORD=`pwgen -1`
 
-cp config/default.toml config/production.toml
+mysql -u root -e "CREATE USER 'mailtrain'@'localhost' IDENTIFIED BY '$MYSQL_PASSWORD';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON mailtrain.* TO 'mailtrain'@'%' WITH GRANT OPTION;"
+mysql -u mailtrain --password="$MYSQL_PASSWORD" -e "CREATE database mailtrain;"
+mysql -u mailtrain --password="$MYSQL_PASSWORD" -D mailtrain < setup/mailtrain.sql
+
+cat >> config/production.toml <<EOT
+[log]
+level="error"
+[www]
+secret="`pwgen -1`"
+[mysql]
+password="$MYSQL_PASSWORD"
+[redis]
+enabled=true
+EOT
+
 echo "running npm install..."
-npm install --no-progress
+npm install --no-progress --production
 echo "npm install done"
