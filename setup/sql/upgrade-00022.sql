e3cb6ca6794e51c2c8befd08b6a617b23f643fac
vladimir <vladimir@nixdi.com>
2017-03-20 17:47:30 +0200

    Fix formating and rewrite migration script

--- setup/sql/upgrade-00022.sql
+++ setup/sql/upgrade-00022.sql
@@ -3,7 +3,36 @@
 SET @schema_version = '22';
 
 # Add field device_type to campaign_tracker
-ALTER TABLE `campaign_tracker` ADD COLUMN `device_type` varchar(50) DEFAULT NULL AFTER `ip`;
+
+# Create ALTER TABLE PROCEDURE
+DROP PROCEDURE IF EXISTS `alterbyregexp`;
+DELIMITER //
+CREATE PROCEDURE alterbyregexp(`table_regexp` VARCHAR(255), `altertext` VARCHAR(255))
+BEGIN
+DECLARE done INT DEFAULT FALSE;
+DECLARE tbl VARCHAR(255);
+DECLARE curs CURSOR FOR SELECT table_name FROM information_schema.tables WHERE table_schema = (SELECT DATABASE()) and table_name like table_regexp;
+DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+OPEN curs;
+
+read_loop: LOOP
+FETCH curs INTO tbl;
+IF done THEN
+  LEAVE read_loop;
+END IF;
+SET @query = CONCAT('ALTER TABLE `', tbl, '`' , altertext);
+PREPARE stmt FROM @query;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+END LOOP;
+CLOSE curs;
+END;
+//
+DELIMITER ;
+
+# Add field device_type to campaign_tracker
+CALL alterbyregexp('campaign\_tracker%', 'ADD COLUMN `device_type` varchar(50) DEFAULT NULL AFTER `ip`');
+DROP PROCEDURE IF EXISTS `alterbyregexp`;
 
 # Footer section
 LOCK TABLES `settings` WRITE;
