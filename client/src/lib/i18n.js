91c059144fdcb9e38abcebd125ed69a270202c4b
bbraganca <49985217+bbraganca@users.noreply.github.com>
2019-05-06 10:12:44 -0300

    Update i18n.js

--- client/src/lib/i18n.js
+++ client/src/lib/i18n.js
@@ -14,11 +14,13 @@ import {createComponentMixin} from "./decorator-helpers";
 
 import lang_en_US_common from "../../../locales/en-US/common";
 import lang_es_ES_common from "../../../locales/es-ES/common";
+import lang_pt_BR_common from "../../../locales/pt-BR/common";
 
 
 const resourcesCommon = {
     'en-US': lang_en_US_common,
     'es-ES': lang_es_ES_common,
+    'pt-BR': lang_pt_BR_common,
     'fk-FK': convertToFake(lang_en_US_common)
 };
 
