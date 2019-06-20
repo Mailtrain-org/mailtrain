70364e8963737f4d604a5447e0be45299b07344a
amit.kumar <amit.kumar@galaxycard.in>
2019-06-20 15:56:31 +0530

    Fix for configmaps

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,61 +1,14 @@
 #!/bin/bash
 set -e
 
-function printHelp {
-    cat <<EOF
-
-Optional parameters:
-  --trustedUrlBase XXX  - sets the trusted url of the instance (default: http://localhost:3000)
-  --sandboxUrlBase XXX  - sets the sandbox url of the instance (default: http://localhost:3003)
-  --publicUrlBase XXX   - sets the public url of the instance (default: http://localhost:3004)
-  --withProxy           - use if Mailtrain is behind an http reverse proxy
-EOF
-
-    exit 1
-}
-
-
-urlBaseTrusted=http://localhost:3000
-urlBaseSandbox=http://localhost:3003
-urlBasePublic=http://localhost:3004
-wwwProxy=false
-
-while [ $# -gt 0 ]; do
-    case "$1" in
-        --help)
-            printHelp
-            ;;
-        --trustedUrlBase)
-            urlBaseTrusted="$2"
-            shift 2
-            ;;
-        --sandboxUrlBase)
-            urlBaseSandbox="$2"
-            shift 2
-            ;;
-        --publicUrlBase)
-            urlBasePublic="$2"
-            shift 2
-            ;;
-        --withProxy)
-            wwwProxy=true
-            shift 1
-            ;;
-        *)
-            echo "Error: unrecognized option $1."
-            printHelp
-    esac
-done
-
-
 cat > server/config/production.yaml <<EOT
 www:
   host: 0.0.0.0
-  proxy: $wwwProxy
+  proxy: true
   secret: "`pwgen -1`"
-  trustedUrlBase: $urlBaseTrusted
-  sandboxUrlBase: $urlBaseSandbox
-  publicUrlBase: $urlBasePublic
+  trustedUrlBase: $TRUSTED_URL
+  sandboxUrlBase: $SANDBOX_URL
+  publicUrlBase: $PUBLIC_URL
 
 mysql:
   host: $MYSQL_HOSTNAME
