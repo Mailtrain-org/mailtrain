8dab13d903e8f19e8d69887977993e84c7768b0b
Tomas Bures <bures@d3s.mff.cuni.cz>
2020-07-25 10:24:34 +0200

    Updated version in Home

--- client/src/Home.js
+++ client/src/Home.js
@@ -25,7 +25,7 @@ export default class List extends Component {
         return (
             <div>
                 <h2>{t('Mailtrain 2 beta')}</h2>
-                <div>{t('Build') + ' 2020-07-18-0813'}</div>
+                <div>{t('Build') + ' 2020-07-25-1024'}</div>
                 <p>{mailtrainConfig.shoutout}</p>
             </div>
         );
